eeb35175f3c47d2df047aa0e6902e7b9c788d62f
Marcos Franco <v.marcos.franco@gmail.com>
2022-08-05 15:27:02 -0300

    config improvements

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,53 +1,57 @@
 // @ts-check
 import { PlaywrightTestConfig, devices } from '@playwright/test';
-import 'dotenv/config'
+import 'dotenv/config';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
 // require('dotenv').config();
 
-
 /**
  * @see https://playwright.dev/docs/test-configuration
  * @type {import('@playwright/test').PlaywrightTestConfig}
  */
- const config: PlaywrightTestConfig  = {
-  globalSetup: require.resolve('./configuration/global-setup'),
+const config: PlaywrightTestConfig = {
+    globalSetup: require.resolve('./configuration/global-setup'),
 
-  use: {
-    baseURL: 'https://www.saucedemo.com',
-    headless: (process.env.NOHEADLESS)? false : true,
-    storageState: 'storageState.json',
-    viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
-    video: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    trace: 'on',
-    launchOptions: {
-      slowMo: 200,
+    use: {
+        baseURL: 'https://www.saucedemo.com',
+        headless: (process.env.NOHEADLESS)? false : true,
+        storageState: 'storageState.json',
+        viewport: { width: 1280, height: 720 },
+        ignoreHTTPSErrors: true,
+        video: 'on-first-retry',
+        screenshot: 'only-on-failure',
+        trace: 'on',
+        launchOptions: {
+            slowMo: 200,
+        },
     },
-  },
-  testDir: 'tests',
+    testDir: 'tests',
 
-  workers: 4,
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'],
-      },
-    },
-  ]
+    reporter: process.env.CI ? [
+        ['dot'],
+        ['html', { open: 'never' }] 
+    ] : 'list',
 
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+    workers: 4,
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'],
+            },
+        },
+    ]
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+    /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+    // outputDir: 'test-results/',
+
+    /* Run your local dev server before starting the tests */
+    // webServer: {
+    //   command: 'npm run start',
+    //   port: 3000,
+    // },
 };
 
 export default config;
